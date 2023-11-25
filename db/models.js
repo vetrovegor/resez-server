30147293ae377d4f2b5c75bf428d2d9a61e3525b
forgetyk <itt2g@mail.ru>
2023-11-25 17:06:58 +0500

    add new models for education

--- db/models.js
+++ db/models.js
@@ -57,8 +57,115 @@ Session.belongsTo(User);
 Token.hasMany(Session);
 Session.belongsTo(Token);
 
+const Subject = sequelize.define('subject', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    subject: { type: DataTypes.STRING },
+    durationMinutes: { type: DataTypes.INTEGER },
+    isMark: { type: DataTypes.BOOLEAN },
+    isPublished: { type: DataTypes.BOOLEAN },
+    isArchive: { type: DataTypes.BOOLEAN, defaultValue: false }
+});
+
+const SubjectTask = sequelize.define('subject_task', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    number: { type: DataTypes.INTEGER },
+    theme: { type: DataTypes.STRING },
+    primaryScore: { type: DataTypes.INTEGER },
+    isDetailedAnswer: { type: DataTypes.BOOLEAN }
+});
+
+const SubTheme = sequelize.define('sub_theme', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    subTheme: { type: DataTypes.STRING }
+});
+
+const Task = sequelize.define('task', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    task: { type: DataTypes.TEXT },
+    solution: { type: DataTypes.TEXT },
+    answer: { type: DataTypes.STRING },
+    isVerified: { type: DataTypes.BOOLEAN },
+    date: { type: DataTypes.DATE }
+});
+
+const Test = sequelize.define('test', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    isPrivate: { type: DataTypes.BOOLEAN },
+    isOfficial: { type: DataTypes.BOOLEAN },
+    isExam: { type: DataTypes.BOOLEAN },
+    date: { type: DataTypes.DATE }
+});
+
+const TestTask = sequelize.define('test_task', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }
+});
+
+const ScoreConversion = sequelize.define('score_conversion', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    primaryScore: { type: DataTypes.INTEGER },
+    secondaryScore: { type: DataTypes.INTEGER },
+    minScore: { type: DataTypes.INTEGER },
+    maxScore: { type: DataTypes.INTEGER },
+    grade: { type: DataTypes.INTEGER },
+    isRed: { type: DataTypes.BOOLEAN, defaultValue: false },
+    isGreen: { type: DataTypes.BOOLEAN, defaultValue: false }
+});
+
+Subject.hasMany(SubjectTask, {
+    foreignKey: 'subjectId',
+    onDelete: 'CASCADE'
+});
+SubjectTask.belongsTo(Subject);
+
+SubjectTask.hasMany(SubTheme, {
+    foreignKey: 'subjectTaskId',
+    onDelete: 'CASCADE'
+});
+SubTheme.belongsTo(SubjectTask);
+
+SubTheme.hasMany(Task, {
+    foreignKey: 'subThemeId',
+    onDelete: 'CASCADE'
+});
+Task.belongsTo(SubTheme);
+
+SubjectTask.hasMany(Task);
+Task.belongsTo(SubjectTask);
+
+Subject.hasMany(Task);
+Task.belongsTo(Subject);
+
+User.hasMany(Task);
+Task.belongsTo(User);
+
+Subject.hasMany(Test, {
+    foreignKey: 'subjectId',
+    onDelete: 'CASCADE'
+});
+Test.belongsTo(Subject);
+
+User.hasMany(Test);
+Test.belongsTo(User);
+
+Test.belongsToMany(Task, { through: TestTask });
+Task.belongsToMany(Test, { through: TestTask });
+
+Subject.hasMany(ScoreConversion, {
+    foreignKey: 'subjectId',
+    onDelete: 'CASCADE'
+});
+ScoreConversion.belongsTo(Subject);
+
+
 export {
     User,
     Token,
-    Session
+    Session,
+    Test,
+    Subject,
+    SubjectTask,
+    SubTheme,
+    Task,
+    TestTask,
+    ScoreConversion
 }
