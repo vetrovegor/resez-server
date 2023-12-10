1c1eac374c265526f1514de15ac468b78bb2eaf7
Сергей Бондаренко <stud0000241179@study.utmn.ru>
2023-12-10 16:50:58 +0500

    create roles, give roles, view users

---
+++ consts/PERMISSIONS.js
@@ -0,0 +1,23 @@
+export const PERMISSIONS = {
+    ADMIN: 'Админка', // 1
+        USERS: 'Пользователи', // 2
+            BLOCK_USERS: 'Блокировка пользователей', // 3
+        NOTIFIES: 'Отпрака уведомлений', // 4
+        ROLES: 'Роли', // 5
+                CREATE_ROLES: 'Создание ролей', // 6
+                UPDATE_ROLES: 'Редактирование ролей', // 7
+                DELETE_ROLES: 'Удаление ролей', // 8
+                ISSUE_ROLES: 'Выдача ролей', // 9
+        TESTS: 'Тесты', // 15
+            CREATE_OFFICIAL_TESTS: 'Создание официальных тестов', // 28
+            DELETE_TESTS: 'Удаление тестов', // 29
+            SUBJECTS: 'Предметы', // 16
+                CREATE_SUBJECTS: 'Создание предметов', // 17
+                UPDATE_SUBJECTS: 'Редактирование предметов', // 18
+                DELETE_SUBJECTS: 'Удаление предметов', // 19
+            TASKS: 'Задания', // 20
+                CREATE_TASKS: 'Создание заданий', // 21
+                UPDATE_TASKS: 'Редактирование заданий', // 22
+                DELETE_TASKS: 'Удаление заданий', // 23
+                VERIFY_TASKS: 'Верификация заданий', // 24
+}
