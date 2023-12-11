d9c2bfa1b0d7f34bf2cac887367438b8190b9d63
Dolgokolenkov <danila.sgd@mail.ru>
2023-12-11 19:23:20 +0500

    add subject routes

--- routes/admin-router.js
+++ routes/admin-router.js
@@ -7,6 +7,9 @@ import { blockedMiddleware } from "../middlewares/blocked-middleware.js";
 import { permissionMiddleware } from "../middlewares/permission-middleware.js";
 import { PERMISSIONS } from "../consts/PERMISSIONS.js";
 import permissionController from "../controllers/permission-controller.js";
+import subjectController from "../controllers/education/subject-controller.js";
+import scoreConversionController from "../controllers/education/score-conversion-controller.js";
+
 
 adminRouter.get('/role',
     paginationMiddleware,
@@ -90,4 +93,90 @@ accessTokenMiddleware,
 blockedMiddleware,
 permissionMiddleware(PERMISSIONS.BLOCK_USERS),
 userController.unblock);
+
+adminRouter.post('/subject',
+    body('subject').isString().notEmpty().isLength({ max: 75 }),
+    body('subjectTasks').isArray({ min: 1 }),
+    body('subjectTasks.*.theme').isString().notEmpty().isLength({ max: 150 }),
+    body('subjectTasks.*.primaryScore').isNumeric(),
+    body('subjectTasks.*.isDetailedAnswer').isBoolean(),
+    body('subjectTasks.*.subThemes').isArray({ min: 1 }),
+    body('subjectTasks.*.subThemes.*').isObject(),
+    body('subjectTasks.*.subThemes.*.subTheme').isString().notEmpty().isLength({ max: 150 }),
+    body('durationMinutes').isNumeric(),
+    body('isMark').isBoolean(),
+    body('isPublished').isBoolean(),
+    subjectTasksMiddleware,
+    accessTokenMiddleware,
+    blockedMiddleware,
+    permissionMiddleware(PERMISSIONS.CREATE_SUBJECTS),
+    subjectController.createSubject);
+
+adminRouter.put('/subject',
+    body('id').isNumeric(),
+    body('subject').isString().notEmpty().isLength({ max: 75 }),
+    body('subjectTasks').isArray({ min: 1 }),
+    body('subjectTasks.*.id').isNumeric().optional(),
+    body('subjectTasks.*.theme').isString().notEmpty().isLength({ max: 75 }),
+    body('subjectTasks.*.primaryScore').isNumeric(),
+    body('subjectTasks.*.isDetailedAnswer').isBoolean(),
+    body('subjectTasks.*.subThemes').isArray({ min: 1 }),
+    body('subjectTasks.*.subThemes.*').isObject(),
+    body('subjectTasks.*.subThemes.*.id').isNumeric().optional(),
+    body('subjectTasks.*.subThemes.*.subTheme').isString().notEmpty().isLength({ max: 75 }),
+    body('durationMinutes').isNumeric(),
+    body('isMark').isBoolean(),
+    body('isPublished').isBoolean(),
+    subjectTasksMiddleware,
+    accessTokenMiddleware,
+    blockedMiddleware,
+    permissionMiddleware(PERMISSIONS.UPDATE_SUBJECTS),
+    subjectController.updateSubjectById);
+
+adminRouter.get('/subject/:id',
+    param('id').isNumeric(),
+    accessTokenMiddleware,
+    blockedMiddleware,
+    permissionMiddleware(PERMISSIONS.SUBJECTS),
+    subjectController.getSubjectFullInfo);
+
+adminRouter.delete('/subject/:id',
+    param('id').isNumeric(),
+    accessTokenMiddleware,
+    blockedMiddleware,
+    permissionMiddleware(PERMISSIONS.DELETE_SUBJECTS),
+    subjectController.deleteSubjectById);
+
+adminRouter.get('/subject',
+    accessTokenMiddleware,
+    blockedMiddleware,
+    permissionMiddleware(PERMISSIONS.SUBJECTS),
+    subjectController.getSubjectShortInfoElements);
+
+adminRouter.get('/subject/:id/score-conversion',
+    accessTokenMiddleware,
+    blockedMiddleware,
+    permissionMiddleware(PERMISSIONS.SUBJECTS),
+    scoreConversionController.getScoreConversionBySubjectId);
+
+adminRouter.get('/subject/:id/score-conversion',
+    accessTokenMiddleware,
+    blockedMiddleware,
+    permissionMiddleware(PERMISSIONS.SUBJECTS),
+    scoreConversionController.getScoreConversionBySubjectId);
+
+adminRouter.post('/score-conversion',
+    body('subjectId').isNumeric(),
+    body('scoreConversion').isArray({ min: 1 }),
+    body('scoreConversion.*.primaryScore').isNumeric().optional(),
+    body('scoreConversion.*.secondaryScore').isNumeric().optional(),
+    body('scoreConversion.*.minScore').isNumeric().optional(),
+    body('scoreConversion.*.maxScore').isNumeric().optional(),
+    body('scoreConversion.*.grade').isNumeric().optional(),
+    body('scoreConversion.*.isRed').isBoolean(),
+    body('scoreConversion.*.isGreen').isBoolean(),
+    accessTokenMiddleware,
+    blockedMiddleware,
+    permissionMiddleware(PERMISSIONS.CREATE_SUBJECTS),
+    scoreConversionController.saveScoreConversion);
 
